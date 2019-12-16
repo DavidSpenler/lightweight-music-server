4c17e35381176875bd661f65bd4d7089b381b22c
DavidSpenler <davidspenler@gmail.com>
2019-12-15 21:46:05 -0500

    Changed frame structure, add functionality to convert problematic string encodings to utf8

--- read_dir.c
+++ read_dir.c
@@ -6,19 +6,16 @@
 #include <stdbool.h>
 #include <arpa/inet.h>
 
-struct string {
-	char* data;
-	int size;
-	char encoding;
-};
+#include "endian.h"
+#include "utf8_encoder.h"
 
 struct metadata {
-	struct string* title;
-	struct string* album;
-	struct string* artist;
-	struct string* year;
-	struct string* track;
-	struct string* genre;
+	char* title;
+	char* album;
+	char* artist;
+	char* year;
+	char* track;
+	char* genre;
 	int duration;
 };
 
@@ -31,9 +28,10 @@ struct id3v2_header {
 
 struct id3v2_frame {
 	char version;
-	char flags;
 	char* id;
-	struct string* data;
+	short flags;
+	int size;
+	char* data;
 };
 
 void truncate_string(char* s) {
@@ -111,13 +109,7 @@ struct id3v2_header* get_id3v2_header(FILE *f) {
 	fread(&(h->flags),1,1,f);
 
 	fread(&tag_size,4,1,f);
-	tag_size = ntohl(tag_size);
-	h->tag_size = (
-		((unsigned char*)&tag_size)[0] << 0 |
-		((unsigned char*)&tag_size)[1] << 7 |
-		((unsigned char*)&tag_size)[2] << 14 |
-		((unsigned char*)&tag_size)[3] << 21
-	);
+	h->tag_size = btomi(tag_size);
 	return h;
 }
 
@@ -128,57 +120,79 @@ struct id3v2_frame* get_id3v2_frame(FILE *f, char v) {
 
 	if (v >= 3) {
 		fr->id = malloc(5);
+		if (fr->id == NULL) fprintf(stderr,"fr->id is null\n");
+		fprintf(stderr,"reading\n");
 		fread(fr->id,1,4,f);
-		if (fr->id[0] == '\0') {
-			free(fr->id);
-			free(fr);
-			return NULL;
-		}
+		fprintf(stderr,"read\n");
 		fr->id[4] = '\0';
+		fprintf(stderr,"set null terminator\n");
 	} else if (v == 2) {
 		fr->id = malloc(4);
 		fread(fr->id,1,3,f);
-		if (fr->id[0] == '\0') {
-			free(fr->id);
-			free(fr);
-			return NULL;
-		}
 		fr->id[3] = '\0';
 	}
+	fprintf(stderr,"checking if null:\n");
+	//check if null
+	if (fr->id[0] == '\0') {
+		fprintf(stderr,"is null, freeing:\n");
+		free(fr->id);
+		fprintf(stderr,"freed id, freeing fr\n");
+		free(fr);
+		return NULL;
+	}
 
-	fr->data = malloc(sizeof(struct string));
+	fprintf(stderr,"id: %s\n",fr->id);
+
 	int frame_size = 0;
 	if (v >= 3) {
 		fread(&frame_size,4,1,f);
-		frame_size = ntohl(frame_size);
 		//exclude encoding byte
-		fr->data->size = (
-			((unsigned char*)&frame_size)[0] << 0 | 
-			((unsigned char*)&frame_size)[1] << 7 | 
-			((unsigned char*)&frame_size)[2] << 14 | 
-			((unsigned char*)&frame_size)[3] << 21 
-		)-1;
+		fr->size = btomi(frame_size)-1;
 	} else if (v == 2) {
 		fread(&frame_size,1,3,f);
-		frame_size = ntohl(frame_size);
-		//exclude encoding byte
-		fr->data->size = (
-			((unsigned char*)&frame_size)[0] << 0 | 
-			((unsigned char*)&frame_size)[1] << 7 | 
-			((unsigned char*)&frame_size)[2] << 14 | 
-		)-1;
+		//exclude encoding byte, compensate for 3-byte input
+		fr->size = (btomi(frame_size) >> 8)-1;
 	}
+
 	if (v >= 3) {
 		fread(&(fr->flags),2,1,f);
+	} else {
+		//come back to this, need to handle nonexistant flags
+		fr->flags = 0;
 	}
+	
+	fprintf(stderr,"got size and flags\n");
 
 	char encoding;
 	fread(&encoding,1,1,f);
+	fprintf(stderr,"encoding: %d\n",encoding);
+	fprintf(stderr,"size: %d\n",fr->size);
+	fr->data = malloc(fr->size);
+	fprintf(stderr,"mallocced data\n");
+	fread(fr->data,1,fr->size,f);
+	fprintf(stderr,"got data\n");
 
-	fr->data->encoding = encoding;
-	fr->data->data = malloc(fr->size+1);
-	fread(fr->data->data,1,fr->size,f);
-	fr->data->data[fr->size] = '\0';
+	if (encoding > 0 && encoding < 3) {
+		fprintf(stderr,"non-utf8 compatible text encoding\n");
+		struct utf8_string* s;
+		//convert from ucs2 to utf8
+		fprintf(stderr,"converting ucs2 string\n");
+		if (encoding == 1) s = ucs2_to_utf8(fr->data,fr->size);
+		//convert from utf16BE to utf8
+		if (encoding == 2) s = utf16be_to_utf8(fr->data,fr->size);
+		fprintf(stderr,"string we got was: %s\n",s->data);
+
+		free(fr->data);
+		fr->data = s->data;
+		fprintf(stderr,"string we got was: %s\n",fr->data);
+		//note: struct id3v2_frame size refers does not include null terminator
+		fr->size = s->length;
+
+		free(s);
+		fprintf(stderr,"string we got was: %s\n",fr->data);
+ 
+	}
+	fprintf(stderr,"got header, returning\n");
 		
 	return fr;
 }
@@ -257,70 +271,74 @@ struct metadata* get_mp3_metadata(char* path) {
 
 			fseek(f,10,SEEK_SET);
 			while (ftell(f) < (long)header->tag_size+10) {
+				fprintf(stderr,"getting frame\n");
 				fr = get_id3v2_frame(f,header->version_major);
-				printf("%s\n",fr->id);
-				printf("%d\n",fr->size);
+				//debug tools - remove later
+				//fprintf(stderr,"%s\n",fr->id);
+				//fprintf(stderr,"%d\n",fr->size);
 				if (fr == NULL) {
 					//reached padding
 					break;
 				}
 				else if (strcmp(fr->id,"TIT2") == 0) {
-					data->title = realloc(data->title,fr->size+1);
-					strcpy(data->title,fr->data);
+					free(data->title);
+					data->title = fr->data;
 				}
 				else if (strcmp(fr->id,"TALB") == 0) {
-					data->album = realloc(data->album,fr->size+1);
-					strcpy(data->album,fr->data);
+					free(data->album);
+					data->album = fr->data;
+					fprintf(stderr,"album title is: %s\n",fr->data);
 				}
 				else if (strcmp(fr->id,"TYER") == 0) {
-					data->year = realloc(data->year,fr->size+1);
-					strcpy(data->year,fr->data);
+					free(data->year);
+					data->year = fr->data;
 					highest_YEAR = 1;
 				}
 				else if (strcmp(fr->id,"TDRC") == 0 && highest_YEAR > 2) {
-					data->year = realloc(data->year,fr->size+1);
-					strcpy(data->year,fr->data);
+					free(data->year);
+					data->year = fr->data;
 					highest_YEAR = 2;
 				}
 				else if (strcmp(fr->id,"TRCK") == 0) {
-					data->track = realloc(data->track,fr->size+1);
-					strcpy(data->track,fr->data);
+					free(data->track);
+					data->track = fr->data;
 				}
 				else if (strcmp(fr->id,"TPE1") == 0) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->data;
 					highest_TPE = 1;
 				}
 				else if (strcmp(fr->id,"TPE2") == 0 && highest_TPE > 2) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->data;
 					highest_TPE = 2;
 				}
 				else if (strcmp(fr->id,"TPE3") == 0 && highest_TPE > 3) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->data;
 					highest_TPE = 3;
 				}
 				else if (strcmp(fr->id,"TPE4") == 0 && highest_TPE > 4) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->data;
 					highest_TPE = 4;
 				}
 				else if (strcmp(fr->id,"TCON") == 0) {
-					//numberic genre code
-					if (fr->data->data[0] == '(') {
-						fr->data[fr->data->size-1]='\0';
-						data->genre = get_genre_string((char)atoi(fr->data->data+1));
+					//numeric genre code
+					if (fr->data[0] == '(') {
+						fr->data[fr->size-1]='\0';
+						data->genre = get_genre_string((char)atoi(fr->data+1));
+						free(fr->data);
 					//string genre
 					} else {
-						data->genre = realloc(data->genre,fr->size+1);
-						strcpy(data->genre,fr->data->data);
+						free(data->genre);
+						data->genre = fr->data;
 					}
+				} else {
+					//don't free data if it's being used
+					free(fr->data);
 				}
-				//printf("s\n",fr->id);
 				free(fr->id);
-				free(fr->->data->data);
-				free(fr->->data);
 				free(fr);
 			}
 		} else if (header->version_major == 2) {
@@ -334,58 +352,62 @@ struct metadata* get_mp3_metadata(char* path) {
 					break;
 				}
 				else if (strcmp(fr->id,"TT2") == 0) {
-					data->title = realloc(data->title,fr->size+1);
-					strcpy(data->title,fr->data);
+					free(data->title);
+					data->title = fr->data;
 				}
 				else if (strcmp(fr->id,"TAL") == 0) {
-					data->album = realloc(data->album,fr->size+1);
-					strcpy(data->album,fr->data);
+					free(data->album);
+					data->album = fr->data;
 				}
 				else if (strcmp(fr->id,"TYE") == 0) {
-					data->year = realloc(data->year,fr->size+1);
-					strcpy(data->year,fr->data);
+					free(data->year);
+					data->year = fr->data;
 				}
 				else if (strcmp(fr->id,"TRK") == 0) {
-					data->track = realloc(data->track,fr->size+1);
-					strcpy(data->track,fr->data);
+					free(data->track);
+					data->track = fr->id;
 				}
 				else if (strcmp(fr->id,"TP1") == 0) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->id;
 					highest_TP = 1;
 				}
 				else if (strcmp(fr->id,"TP2") == 0 && highest_TP > 2) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->id;
 					highest_TP = 2;
 				}
 				else if (strcmp(fr->id,"TP3") == 0 && highest_TP > 3) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->id;
 					highest_TP = 3;
 				}
 				else if (strcmp(fr->id,"TP4") == 0 && highest_TP > 4) {
-					data->artist = realloc(data->artist,fr->size+1);
-					strcpy(data->artist,fr->data);
+					free(data->artist);
+					data->artist = fr->id;
 					highest_TP = 4;
 				}
 				else if (strcmp(fr->id,"TCO") == 0) {
 					//numeric genre code
-					if (fr->data->data[0] == '(') {
-						fr->data->data[fr->data->size-1]='\0';
-						data->genre = get_genre_string((char)atoi(fr->data->data+1));
+					if (fr->data[0] == '(') {
+						fr->data[fr->size-1]='\0';
+						data->genre = get_genre_string((char)atoi(fr->data+1));
+						free(fr->data);
 					//string genre
 					} else {
-						data->genre = realloc(data->genre,fr->size+1);
-						strcpy(data->genre,fr->data->data);
+						free(data->genre);
+						data->genre = fr->data;
 					}
+				} else {
+					//don't free data if it's being used
+					free(fr->data);
 				}
 				free(fr->id);
-				free(fr->data);
 				free(fr);
 			}
 		}
 		free(header);
+	}
 	
 	fclose(f);
 	return data;
@@ -460,7 +482,7 @@ int parse_dir(char* base_dir) {
 			dir = opendir(base_dir);
 			seekdir(dir,loc);
 		} else if (S_ISREG(ent_stat.st_mode)) {
-			printf("%s\n",file_name);
+			fprintf(stderr,"%s\n",file_name);
 			for (char* c = file_name;*c;c++) {
 				if (*c == '.') file_ext = c;
 			}
@@ -478,7 +500,7 @@ int parse_dir(char* base_dir) {
 				data = get_flac_metadata(sub_dir); 	
 				free(data);
 			}
-			printf("done\n");
+			fprintf(stderr,"done\n");
 		}
 		ent = readdir(dir);
 	}
